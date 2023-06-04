878b6ca16c8f50fa4da3b303831e2f1cbb454e44
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2023-06-03 22:33:12 -0300

    api com todas as rotas funcionando

--- src/controllers/tutorController.ts
+++ src/controllers/tutorController.ts
@@ -26,6 +26,27 @@ export const createTutor = (req: Request, res: Response) =>{
     saveTutorData(tutors);
     return res.status(201).json(newTutor);
 };
+export const createPet = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const newPet = req.body;
+
+  const tutors = readTutorData();
+  const tutor = tutors.find((tutor: any) => tutor.id === parseInt(id));
+
+  if (!tutor) {
+    return res.status(404).json({ error: 'Tutor not found' });
+  }
+
+  const petId = tutor.pets.length + 1;
+  newPet.id = petId;
+
+  tutor.pets.push(newPet);
+  saveTutorData(tutors);
+
+  return res.status(201).json(newPet);
+};
+
+
 export const updateTutor = (req: Request, res: Response) => {
   const { id } = req.params;
   const updatedTutor = req.body;
@@ -36,12 +57,34 @@ export const updateTutor = (req: Request, res: Response) => {
   if (!tutor) {
     return res.status(404).json({ error: 'Tutor not found' });
   }
-
   // Atualize as propriedades do tutor com base nos dados fornecidos
   Object.assign(tutor, updatedTutor);
   saveTutorData(tutors);
 
   return res.json(tutor);
+};
+export const updatePet = (req: Request, res: Response) => {
+  const { petId, tutorId } = req.params;
+  const updatedPet = req.body;
+
+  const tutors = readTutorData();
+  const tutor = tutors.find((tutor: any) => tutor.id === parseInt(tutorId));
+
+  if (!tutor) {
+    return res.status(404).json({ error: 'Tutor not found' });
+  }
+
+  const pet = tutor.pets.find((pet: any) => pet.id === parseInt(petId));
+
+  if (!pet) {
+    return res.status(404).json({ error: 'Pet not found' });
+  }
+
+  // Atualize as propriedades do pet com base nos dados fornecidos
+  Object.assign(pet, updatedPet);
+  saveTutorData(tutors);
+
+  return res.json(pet);
 };
 export const deleteTutor = (req: Request, res: Response) => {
   const { id } = req.params;
@@ -59,6 +102,29 @@ export const deleteTutor = (req: Request, res: Response) => {
 
   return res.json(deletedTutor);
 };
+export const deletePet = (req: Request, res: Response) => {
+  const { tutorId, petId } = req.params;
+
+  const tutors = readTutorData();
+  const tutor = tutors.find((tutor: any) => tutor.id === parseInt(tutorId));
+
+  if (!tutor) {
+    return res.status(404).json({ error: 'Tutor not found' });
+  }
+
+  const petIndex = tutor.pets.findIndex((pet: any) => pet.id === parseInt(petId));
+
+  if (petIndex === -1) {
+    return res.status(404).json({ error: 'Pet not found' });
+  }
+
+  const deletedPet = tutor.pets.splice(petIndex, 1)[0];
+  saveTutorData(tutors);
+
+  return res.json(deletedPet);
+};
+
+
 
 
 
--- src/routes/tutorRoutes.ts
+++ src/routes/tutorRoutes.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import express from 'express';
-import { createTutor, getAllTutors, updateTutor, deleteTutor } from '../controllers/tutorController';
+import { createTutor, getAllTutors, updateTutor, deleteTutor, createPet, updatePet, deletePet} from '../controllers/tutorController';
 
 const router = express.Router();
 router.use(express.json())
@@ -8,15 +8,18 @@ router.use(express.json())
 
 router.get('/tutor', getAllTutors, (req: Request, res: Response) => {
 });
-
 router.post('/tutor', createTutor, (req: Request, res: Response) => {
 });
-
 router.put('/tutor/:id',updateTutor, (req: Request, res: Response) => {
 });
 router.delete('/tutor/:id', deleteTutor, (req: Request, res: Response) =>{
 });
-
+router.post('/pet/:id', createPet, (req: Request, res: Response) =>{
+});
+router.put('/pet/:petId/tutor/:tutorId', updatePet, (req: Request, res: Response) =>{
+});
+router.delete('/pet/:petId/tutor/:tutorId', deletePet, (req: Request, res: Response) =>{
+});
 
 
 export default router;
